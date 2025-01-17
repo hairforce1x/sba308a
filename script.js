85d2ac9daed442f1a2b840a93523d1b957ab7024
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-01-17 16:39:12 -0700

    Joke is posting to page on load.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const apiUrl = "https://v2.jokeapi.dev/joke/any"
+const displaySetup = document.getElementById("joke-setup");
+const displayDelivery = document.getElementById("joke-delivery");
 
-
-async function getJoke(){ // fetches random joke
+async function getJoke() { // fetches random joke
     try {
         const response = await fetch(apiUrl);
         const joke = await response.json();
+        console.log(joke)
+        if (joke.type === 'single') {
+            displaySetup.textContent = `${joke.joke}`;
+        } else {
+            displaySetup.textContent = `${joke.setup}`;
+            displayDelivery.textContent = `${joke.delivery}`;
+        }
     } catch (error) {
         console.log(error)
     }
 }
-fetchJoke();
+getJoke()
