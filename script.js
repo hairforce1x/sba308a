1461c72659c3f10a70da70f007ff9efc8cf50cbc
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-01-17 17:05:34 -0700

    Added array to save joke object received.

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const apiUrl = "https://v2.jokeapi.dev/joke/any"
 const displaySetup = document.getElementById("joke-setup");
 const displayDelivery = document.getElementById("joke-delivery");
+const savedJokes = document.getElementById("saved-jokes");
 const button = document.querySelector("button");
+let jokesArray = [];
+
 
 async function getJoke() { // fetches random joke
     try {
         const response = await fetch(apiUrl);
         const joke = await response.json();
-        console.log(joke)
+        // console.log(joke)
         if (joke.type === 'single') {
             displaySetup.textContent = `${joke.joke}`;
+            jokesArray.push(joke);
         } else {
             displaySetup.textContent = `${joke.setup}`;
             displayDelivery.textContent = `${joke.delivery}`;
+            jokesArray.push(joke);
+            console.log(jokesArray)
+            console.log(jokesArray[1])
         }
     } catch (error) {
         console.log(error)
@@ -23,6 +30,5 @@ async function getJoke() { // fetches random joke
 button.addEventListener("click", handleClick);
 
 function handleClick(){
-    console.log("clicked")
     getJoke()
 }
