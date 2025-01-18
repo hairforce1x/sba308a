0b5531f4bd9a79020374e87a436ff19411a45092
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-01-17 19:38:52 -0700

    Save Joke button functionality added.

--- script.js
+++ script.js
@@ -7,36 +7,42 @@ const saveButton = document.getElementById("save-btn");
 let jokesArray = [];
 let currentJoke;
 
-
 async function getJoke() { // fetches random joke
     try {
         const response = await fetch(apiUrl);
         const joke = await response.json();
-        // console.log(joke)
-        if (joke.type === 'single') {
-            displaySetup.textContent = `${joke.joke}`;
-        } else {
-            displaySetup.textContent = `${joke.setup}`;
-            displayDelivery.textContent = `${joke.delivery}`;
-        }
         currentJoke = joke;
-        console.log(currentJoke.flags[3])
+        jokesArray.push(currentJoke);
+        // console.log(jokesArray);
+        displayJoke()
+        // console.log(currentJoke);
     } catch (error) {
         console.log(error)
     }
 }
-
+function displayJoke() {
+    if (currentJoke.type === 'single') {
+        displaySetup.textContent = `${currentJoke.joke}`;
+    } else {
+        displaySetup.textContent = `${currentJoke.setup}`;
+        displayDelivery.textContent = `${currentJoke.delivery}`;
+    }
+}
 reqButton.addEventListener("click", handleClick);
 
-function handleClick(){
+async function handleClick() {
     getJoke()
+    await console.log(jokesArray);
 }
 
 saveButton.addEventListener("click", saveClick);
 
-function saveClick(){
-    let newOption = document.createElement("option");
-    newOption.value = ''
+function saveClick() {
+    const savedJokes = document.getElementById("saved-jokes");
+    const newOption = document.createElement("option");
+    console.log(currentJoke.setup)
+    newOption.textContent = `${currentJoke.setup}`
+
     savedJokes.appendChild(newOption)
     console.log(savedJokes)
 }
